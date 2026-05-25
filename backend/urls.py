79527864055a66cf202d1f46ffe8cebaa574359c
Tristan <tristanm15estrada@gmail.com>
2026-05-25 18:27:07 -0500

    fix: configuracion login JWT

--- backend/urls.py
+++ backend/urls.py
@@ -9,13 +9,9 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/login/',
-         TokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('api/token/refresh/',
-         TokenRefreshView.as_view(),
-         name='token_refresh'),
 
     path('api/', include('usuarios.urls')),
     path('api/', include('tareas.urls')),
